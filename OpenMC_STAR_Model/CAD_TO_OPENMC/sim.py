0bc6bb5a6687959378d824c10acb87e63764e2fb
Rocco <698rocco@gmail.com>
2026-02-02 17:14:13 -0500

    added tests and various configs for steps

--- OpenMC_STAR_Model/CAD_TO_OPENMC/sim.py
+++ OpenMC_STAR_Model/CAD_TO_OPENMC/sim.py
@@ -2,8 +2,9 @@
 
 import CAD_to_OpenMC.assembly as ab
 
-A=ab.Assembly(['STAR5_00_1.STEP'])
+A=ab.Assembly(['STAR5_Checked242.STP'])
 ab.mesher_config['threads']=1
 ab.mesher_config['tolerance']=1e-2
 ab.mesher_config['angular_tolerance']=1e-2
-A.run(h5m_filename='STAR5_00_1.h5m', backend='stl2')
+A.set_tag_delim("\s_@")
+A.run(h5m_filename='output.h5m', backend='stl2')

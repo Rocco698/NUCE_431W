5cfd893a5bd9d25bba2c4d5bb7ecdd2ce8bcd0b1
v-ivak <vivakimov@gmail.com>
2026-02-05 12:30:58 -0500

    Update and rename Setting&Tallies_CapStone.py to Setting&Tallies.py

--- OpenMC_STAR_Model/Setting&Tallies.py
+++ OpenMC_STAR_Model/Setting&Tallies.py
@@ -27,6 +27,7 @@ my_sources = tokamak_source(
     triangularity=0.270,
     fuel={"D": 0.5, "T": 0.5}, # Ask Khodak about all parameters
 )
+my_sources = openmc.IndependentSource()
 
 # Option 2
 
@@ -36,19 +37,47 @@ my_source = fusion_ring_source(
     temperature=20000.0, # also needs to be modified
     fuel={"D": 0.5, "T": 0.5}, # Ask Khodak
 )
-
+my_source = openmc.IndependentSource()
 
 
 
 settings = openmc.Settings()
 settings.run_mode = 'fixed source'
-# Add line specifiying which source we are using
+settings.source = my_source or my_sources # pick one
 settings.particles = 10000
 settings.generations_per_batch = 20
 settings.batches = 200
 settings.inactive = 50
+settings.output = {'tallies':True, 'path': 'results'}
 
-cell_filer = openmc.CellFileter([blanket.id, sheilding.id])
-tally.scores['absorption', 'H3-production']
+cell_filter = openmc.CellFilter([blanket.id])
+tbr1_tally = openmc.Tally
+tbr1_tally.scores['absorption', 'H3-production']
+tbr1_tally.filters = [cell_filter]
+
 # Tallies must be multiplied by our given source strength
 # H3-production/absorption = TBR
+
+# Second method, may be more accurate
+
+# creates a mesh that covers the geometry
+mesh = openmc.RegularMesh()
+mesh.dimension = [100, 100, 100]
+mesh.lower_left = [0, 0, -350]  # x,y,z coordinates start at 0 as this is a sector model
+mesh.upper_right = [650, 650, 350]
+mesh_filter = openmc.MeshFilter(mesh) # creating a mesh
+
+# adds a tally to record the total TBR
+tbr_cell_tally = openmc.Tally(name="tbr")
+tbr_cell_tally.scores = ["(n,Xt)"]
+
+# makes a mesh tally using the previously created mesh and records TBR on the mesh
+tbr_mesh_tally = openmc.Tally(name="tbr_on_mesh")
+tbr_mesh_tally.filters = [mesh_filter]
+tbr_mesh_tally.scores = ["(n,Xt)"]
+
+tallies = openmc.Tallies([tbr_cell_tally, tbr_mesh_tally, tbr1_tally]) 
+
+my_model = openmc.Model(materials=materials, geometry=geometry, settings=settings, tallies=tallies)
+my_model.run()
+

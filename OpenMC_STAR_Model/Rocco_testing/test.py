3ffaece828e2b7102ef6bb72e1441e64777386cd
Rocco <698rocco@gmail.com>
2026-03-03 18:47:51 -0500

    updated testing code and such. NOTE: re-running mesher. DAGMC cannot successfully seal the model, must make tolerance tighter. Source works and all other functions work inside test.py

--- OpenMC_STAR_Model/Rocco_testing/test.py
+++ OpenMC_STAR_Model/Rocco_testing/test.py
@@ -18,21 +18,17 @@ import urllib.request
 # ##############################################
 #       MATERIALS
 # ##############################################
+
 #Material Initialization
 Steel_material = openmc.Material(name='Steel_material')
 Shielding_material = openmc.Material(name='Shielding_material')
 Breeder_material = openmc.Material(name='Breeder_material')
 Coolant_material = openmc.Material(name='Coolant_material')
 plasma_material = openmc.Material(name = 'Plasma_Material')
-fuel = openmc.Material(name = 'fuel')
 
 # plasma_material (void)
 plasma_material.add_element('Ar', 1.0)
-plasma_material.set_density('g/cm3', 1.00)
-
-# fuel?
-fuel.add_nuclide('H3', 0.5, 'wo')
-fuel.add_nuclide('H2', 0.5, 'wo')
+plasma_material.set_density('g/cm3', 0.00000000000000001)
 
 #Steel-EUROFER97
 Steel_material.add_element('Fe', 0.8924, 'wo')
@@ -79,7 +75,7 @@ Breeder_material.set_density('g/cm3',2.35)
 Coolant_material.add_element('He',1.0,'ao')
 Coolant_material.set_density('kg/m3',5.0)
 
-mat_list= openmc.Materials([Steel_material, Shielding_material, Breeder_material, Coolant_material, plasma_material, fuel])
+mat_list= openmc.Materials([Steel_material, Shielding_material, Breeder_material, Coolant_material, plasma_material])
 mat_list.export_to_xml()
 
 mat_list.cross_sections = "/Users/rocco698/Desktop/JENDL5/jendl-5-hdf5/cross_sections.xml"
@@ -92,9 +88,23 @@ print('materials export success')
 
 dag_univ = openmc.DAGMCUniverse('/Users/rocco698/Desktop/Undergrad/Spring_2026/NUCE_431W/NUCE_431W/OpenMC_STAR_Model/CAD_TO_OPENMC/STAR5_Whole.h5m')
 
-sim_univ = dag_univ.bounded_universe(boundary_type = 'reflective', padding_distance = 30.0)
+cell_count = dag_univ.n_cells
 
-plasma_cell = openmc.Cell(cell_id = 63, name = 'Plasma Region')
+print()
+print(f"Number of Cells within Model: {cell_count}")
+print()
+
+print()
+print(f"Path to Model:  {dag_univ.filename}")
+print()
+
+print()
+print(f"Cells: {dag_univ.get_all_cells}")
+
+
+
+
+sim_univ = dag_univ.bounded_universe(bounding_cell_id= 999, boundary_type = 'reflective', padding_distance = 30.0)
 
 
 geometry = openmc.Geometry(sim_univ)
@@ -106,12 +116,65 @@ print(mat_list)
 #       SOURCE DEFINITION
 # #################################################
 
-plasma_source = fusion_ring_source(
-    radius = 700,
-    angles = (0.0, 2 * np.pi),
-    temperature = 20000.0,
-    fuel={"D": 0.5, "T": 0.5},
-)
+# Heavy use of code from: https://github.com/fusion-energy/openmc-plasma-source/blob/main/examples/ring_source_example.py
+def onion_ring_source(radius: float, z_placement: float, activity: float, #these are the only inputs you should need to change
+                      angles: Tuple[float, float] = (0, 2 * np.pi),       #not these
+                      fuel: Dict = {"D": 0.5, "T": 0.5}):                 #not these
+    """Creates a list of openmc.IndependentSource objects in a ring shape.
+
+    Useful for simulations where all the plasma parameters are not known and
+    this simplified geometry will suffice. Resulting ring source will have an
+    energy distribution according to the fuel composition.
+    Args:
+        radius: the inner radius of the ring source, in metres
+        angles: the start and stop angles of the ring in radians
+       z_placement: Location of the ring source (m). Defaults to 0.
+        temperature: Temperature of the source (eV). #Unused#
+        fuel: Isotopes as keys and atom fractions as values
+    Returns:
+        A list of one openmc.IndependentSource instance.
+    """
+    if not isinstance(radius, (int, float)) or radius <= 0:
+        raise ValueError("Radius must be a float strictly greater than 0.")
+    if not (
+        isinstance(angles, tuple)
+        and len(angles) == 2
+        and all(
+            isinstance(angle, (int, float)) and -2 * np.pi <= angle <= 2 * np.pi
+            for angle in angles
+        )
+    ):
+        raise ValueError("Angles must be a tuple of floats between zero and 2 * np.pi")
+    if not isinstance(z_placement, (int, float)):
+        raise TypeError("Z placement must be a float.")
+    #if not (isinstance(temperature, (int, float)) and temperature > 0): #Temp not used, assumed 14 MeV
+        #raise ValueError("Temperature must be a float strictly greater than 0.")
+    source = IndependentSource()
+    source.space = openmc.stats.CylindricalIndependent(
+        r=openmc.stats.Discrete([radius], [1]),
+        phi=openmc.stats.Uniform(a=angles[0], b=angles[1]),
+        z=openmc.stats.Discrete([z_placement], [1]),
+        origin=(0.0, 0.0, 0.0) )
+    source.energy =openmc.stats.Discrete([14.0e6], [1.0]) # (14 MeV neutrons, 100% distribution)
+    source.angle = openmc.stats.Isotropic()
+    source.strength = activity
+    return source
+
+# Create data frame from excel sheet #
+excel_path = "/Users/rocco698/Desktop/Undergrad/Spring_2026/NUCE_431W/NUCE_431W/OpenMC_STAR_Model/STAR40_Neutonics_Data.xlsx"
+df = pd.read_excel(excel_path)
+radi_s = df.loc[:,"R [m]"].tolist()
+z_pos = df.loc[:,"Z[m]"].tolist()
+norm_activ = df.loc[:,"norm"].tolist()
+
+#sources = onion_ring_source(radius= radi_s[0], z_placement= z_pos[0], activity= norm_activ[0])
+
+iter=0
+sources = []
+while iter <= 501:
+    sources.append(onion_ring_source(radius=radi_s[iter], z_placement=z_pos[iter], activity=norm_activ[iter]))
+    iter += 1
+print('> Sources array:', sources)
 
 
 # Create data frame from excel sheet #
@@ -125,6 +188,12 @@ norm_activ = df.loc[:,"norm"].tolist()
 #       TALLIES
 # #################################################
 
+#tallies_file = openmc.Tallies()
+
+#flux_map_mesh = openmc.RegularMesh()
+
+
+
 ###############################################################################
 # Define problem settings
 ###############################################################################
@@ -134,7 +203,7 @@ settings.run_mode = 'fixed source'
 settings.dagmc = True
 settings.batches = 10
 settings.particles = 5000
-settings.source = plasma_source
+settings.source = sources
 settings.export_to_xml()
 
 print(settings)
@@ -148,6 +217,12 @@ plot1 = openmc.Plot()
 plot1.width = (ww,ww)
 plot1.basis = 'yz'
 plot1.color_by = 'material'
+plot1.colors = {
+    Steel_material: 'black',
+    Shielding_material: 'deeppink',
+    Breeder_material: 'blue',
+    plasma_material: 'red',
+}
 plot1.filename = 'TestImg'
 plot1.pixels = (900,900)
 plots = openmc.Plots([plot1])
@@ -156,5 +231,5 @@ plots.export_to_xml()
 # Set the environment variable for cross sections
 os.environ["OPENMC_CROSS_SECTIONS"] = "/Users/rocco698/Desktop/JENDL5/jendl-5-hdf5/cross_sections.xml"
 
-openmc.plot_geometry()
+#openmc.plot_geometry()
 openmc.run()
